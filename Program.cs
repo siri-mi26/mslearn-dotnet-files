881fa7234fe6a7dff9d0358b9eff0d5ba78320ee
siri-mi26 <sirimissula@gmail.com>
2023-01-11 07:42:16 +0800

    Working file traversal of sales.json

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+using System.Collections.Generic;
+var salesFiles = FindFiles("stores");
+
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        // The file name will contain the full path, so only check the end of it
+        if (file.EndsWith("sales.json"))
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
