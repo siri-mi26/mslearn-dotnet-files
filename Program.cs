3841ed95110479534a8c45a71aad2cba05d57672
siri-mi26 <sirimissula@gmail.com>
2023-01-11 08:05:22 +0800

    created new salesTotal file

--- Program.cs
+++ Program.cs
@@ -3,12 +3,17 @@ using System.IO;
 using System.Collections.Generic;
 var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
-var salesFiles = FindFiles(storesDirectory);
 
-foreach (var file in salesFiles)
-{
-    Console.WriteLine(file);
-}
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);
+
+var salesFiles = FindFiles(storesDirectory);
+File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+
+// foreach (var file in salesFiles)
+// {
+//     Console.WriteLine(file);
+// }
 IEnumerable<string> FindFiles(string folderName)
 {
     List<string> salesFiles = new List<string>();
